5807024c21701c7a2471d6edb3716ab42f72a72a
cristinasebastian <cristinanunezsebastian@gmail.com>
2022-04-29 14:51:34 +0200

    Añadir estilos con tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,27 @@ export function App() {
 
   return (
     <>
-    <img src={logo} width="300" />
-      <h1>SpaceX Launches</h1>
-      <ul>
+    <img src={logo} class="m-4 w-72" />
+      <h1 class="m-4 text-3xl font-bold">SpaceX Launches</h1>
+      <section>
         {launches.map(launch =>(
-          <li key={`${launch.flight_number}_${launch.mission_name}`}>
-            {launch.mission_name} ({launch.launch_year})
-          </li>
+          <div key={`${launch.flight_number}_${launch.mission_name}`} class="p-4 mt-1 mx-1 w-1/2 flex flex-col rounded-lg dark:bg-gray-100">
+            <div>
+              <p class="text-2xl">
+                Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+              </p>
+              <div class="flex justify-between">
+                <div>
+                  {launch.mission_name} ({launch.launch_year})
+                </div>
+                <div class={`w-20 h-7 text-center font-bold ${launch.launch_success ? "rounded-lg dark:bg-green-200 dark:text-green-800" : "rounded-lg dark:bg-red-200 dark:text-red-800"} `}>
+                  {launch.launch_success ? "Success" : "Failure"}
+                </div>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </section>
     </>
   )
 }
